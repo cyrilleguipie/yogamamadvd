6074b3472b2ddc1e0a3c6cf2578d89105860fbe6
azhdanov <azhdanov@47e8cd93-a071-7289-a944-3e509be4b2fb>
2011-10-31 23:15:49 +0000

    faq

---
+++ trunk/catalog/language/russian/information/faq.php
@@ -0,0 +1,13 @@
+<?php
+// Heading 
+$_['heading_title']    = 'Вопросы и ответы';
+
+// Text
+$_['text_faq']	       = 'В начало';
+$_['text_error']	   = 'Станица не найдена';
+$_['text_description'] = '<p>Вопросы и ответы!</p>';
+
+// Buttons
+$_['button_faq']       = 'В начало';
+$_['button_more']       = 'Eще';
+?>
