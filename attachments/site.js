4d932102f4fb6e6b7f23778edf4451ff73935355
azhdanov <azhdanov@gmail.com>
2011-11-04 16:26:37 +0000

    try knockout

--- attachments/site.js
+++ attachments/site.js
@@ -45,18 +45,52 @@ var param = function( a ) {
 	}
 }
 
-var app = {baseURL: /* util.getBaseURL(document.location.pathname) */ 'http://li.iriscouch.com/cats/_design/app/_rewrite/'};
+var app = {baseURL: /* util.getBaseURL(document.location.pathname) */ 'http://li.iriscouch.com/cats/_design/app/_rewrite'};
 app.index = function () {
-  request({url: app.baseURL + 'api'}, function(error, data) {
+  request({url: app.baseURL + '/_view/foo'}, function(error, data) {
     app.s.swap($('script#test').tmpl(data));
+    $(data.rows).each(function(i, row) {
+        $('input#' + row.id).data('doc', row.value);
+    })
   })
 };
 
-$(function () { 
+app.update = function (item) {
+    var doc = $(item).data('doc');
+    doc.name = item.value;
+    request({type: 'PUT', url: app.baseURL + '/api/' + item.id, data: doc}, function(error, data) {
+    })
+}
+
+/*
+$(function () {
   app.s = $.sammy('body', function () {
     // Index of all databases
     this.get('', app.index);
     this.get("#/", app.index);
   })
   app.s.run();
+});
+*/
+
+// The view model is an abstract description of the state of the UI, but without any knowledge of the UI technology (HTML)
+var viewModel = {};
+
+var save = function (row) {
+  request({type: 'PUT', url: app.baseURL + '/api/' + row._id, data: row}, function(error, data) {
+  })
+};
+
+request({url: app.baseURL + '/_view/foo'}, function(error, data) {
+  viewModel.rows = ko.observableArray(); //ko.observableArray(data.rows);
+  $(data.rows).each(function(i, row) {
+    var o = ko.observable(row.value);
+    viewModel.rows.push(o);
+    o.subscribe(function() {
+        alert(this)});
+  })
+
+  viewModel.rows.subscribe(save);
+
+  ko.applyBindings(viewModel);
 });
