b2f41425fb0ea82db88fa846f426ca918cfd066d
azhdanov@gmail.com <azhdanov@gmail.com@47e8cd93-a071-7289-a944-3e509be4b2fb>
2011-11-07 17:36:23 +0000

    crud

--- branches/couchapp/attachments/site.js
+++ branches/couchapp/attachments/site.js
@@ -1,48 +1,60 @@
 var request = function (options, callback) {
-  options.success = function (obj) {
-    callback(null, obj);
-  }
-  options.error = function (err) {
-    if (err) callback(err);
-    else callback(true);
-  }
-  if (options.data && typeof options.data == 'object') {
-    options.data = JSON.stringify(options.data)
-  }
-  if (!options.dataType) options.processData = false;
-  if (!options.dataType) options.contentType = 'application/json';
-  if (!options.dataType) options.dataType = 'json';
-  $.ajax(options)
+    options.success = function (obj) {
+        callback(null, obj);
+    }
+    options.error = function (err) {
+        if (err) callback(err);
+        else callback(true);
+    }
+    if (options.data && typeof options.data == 'object') {
+        options.data = JSON.stringify(options.data)
+    }
+    if (!options.dataType) options.processData = false;
+    if (!options.dataType) options.contentType = 'application/json';
+    if (!options.dataType) options.dataType = 'json';
+    $.ajax(options)
 }
 
-$.expr[":"].exactly = function(obj, index, meta, stack){ 
-  return ($(obj).text() == meta[3])
+$.expr[":"].exactly = function(obj, index, meta, stack) {
+    return ($(obj).text() == meta[3])
 }
 
-var param = function( a ) {
-  // Query param builder from jQuery, had to copy out to remove conversion of spaces to +
-  // This is important when converting datastructures to querystrings to send to CouchDB.
-	var s = [];
-	if ( jQuery.isArray(a) || a.jquery ) {
-		jQuery.each( a, function() { add( this.name, this.value ); });		
-	} else { 
-	  for ( var prefix in a ) { buildParams( prefix, a[prefix] ); }
-	}
-  return s.join("&");
-	function buildParams( prefix, obj ) {
-		if ( jQuery.isArray(obj) ) {
-			jQuery.each( obj, function( i, v ) {
-				if (  /\[\]$/.test( prefix ) ) { add( prefix, v );
-				} else { buildParams( prefix + "[" + ( typeof v === "object" || jQuery.isArray(v) ? i : "") +"]", v )}
-			});				
-		} else if (  obj != null && typeof obj === "object" ) {
-			jQuery.each( obj, function( k, v ) { buildParams( prefix + "[" + k + "]", v ); });				
-		} else { add( prefix, obj ); }
-	}
-	function add( key, value ) {
-		value = jQuery.isFunction(value) ? value() : value;
-		s[ s.length ] = encodeURIComponent(key) + "=" + encodeURIComponent(value);
-	}
+var param = function(a) {
+    // Query param builder from jQuery, had to copy out to remove conversion of spaces to +
+    // This is important when converting datastructures to querystrings to send to CouchDB.
+    var s = [];
+    if (jQuery.isArray(a) || a.jquery) {
+        jQuery.each(a, function() {
+            add(this.name, this.value);
+        });
+    } else {
+        for (var prefix in a) {
+            buildParams(prefix, a[prefix]);
+        }
+    }
+    return s.join("&");
+    function buildParams(prefix, obj) {
+        if (jQuery.isArray(obj)) {
+            jQuery.each(obj, function(i, v) {
+                if (/\[\]$/.test(prefix)) {
+                    add(prefix, v);
+                } else {
+                    buildParams(prefix + "[" + ( typeof v === "object" || jQuery.isArray(v) ? i : "") + "]", v)
+                }
+            });
+        } else if (obj != null && typeof obj === "object") {
+            jQuery.each(obj, function(k, v) {
+                buildParams(prefix + "[" + k + "]", v);
+            });
+        } else {
+            add(prefix, obj);
+        }
+    }
+
+    function add(key, value) {
+        value = jQuery.isFunction(value) ? value() : value;
+        s[ s.length ] = encodeURIComponent(key) + "=" + encodeURIComponent(value);
+    }
 }
 
 var app = {baseURL: /* util.getBaseURL(document.location.pathname) */ 'http://li.iriscouch.com/cats/_design/app/_rewrite'};
@@ -60,73 +72,103 @@ app.uuid = function (callback) {
     }
 };
 
-ko.bindingHandlers.addOnEnter = {
-    init: function(element, valueAccessor) {
+ko.bindingHandlers.editing = {
+    init: function(element, valueAccessor, allBindingsAccessor) {
+        $(element).focus(function() {
+            var value = valueAccessor();
+            value(true);
+        });
+        $(element).blur(function() {
+            var value = valueAccessor();
+            value(false);
+        });
         $(element).keypress(function(event) {
-          if ( event.which == 13 ) {
-             event.preventDefault();
-             var doc = {
-               name: event.target.value,
-               type: 'todo',
-               completed: false
-             };
-             app.uuid(function(uuid) {
-               doc._id = uuid;
-               save(ko.toJS(doc), function(error, data) {
-                 doc._rev = data.rev;
-               })             
-             })
-           }
-        })
+            if (event.which == 13) {
+                element.blur();
+            } else if (event.keyCode == 27) {
+                var allBindings = allBindingsAccessor();
+                var oldValue = allBindings.value();
+                element.value = oldValue;
+                element.blur();
+            }
+        });
+    },
+    update: function(element, valueAccessor) {
+        var value = valueAccessor();
+        if (ko.utils.unwrapObservable(value)) {
+            element.focus();
+            // position cursor to text end
+            var $tmp = element.value;
+            element.value = '';
+            element.value = $tmp;
+        } else {
+            element.blur();
+        }
     }
 };
 
 // The view model is an abstract description of the state of the UI, but without any knowledge of the UI technology (HTML)
-var viewModel = {};
+var viewModel = {
+    title: ko.observable('Test'),
+    editingTitle: ko.observable(false),
+    newItem: ko.observable(''),
+    editingNewItem: ko.observable(false),
+    about: ko.observable(false)
 
-var save = function (doc, callback) {
-  request({type: 'PUT', url: app.baseURL + '/api/' + doc._id, data: doc}, callback)
 };
 
-var create = function() {
-  
+viewModel.newItem.subscribe(function(newValue) {
+    if ($.trim(newValue) == '') {
+        return;
+    }
+    var doc = {
+        name: newValue,
+        type: 'todo',
+        completed: false
+    };
+    app.uuid(function(uuid) {
+        doc._id = uuid;
+        viewModel.save(ko.toJS(doc), function(error, data) {
+            doc._rev = data.rev;
+            viewModel.rows.push(observable(doc));
+            viewModel.newItem(''); // clear
+        })
+    })
+});
+
+viewModel.save = function (doc, callback) {
+    request({type: 'PUT', url: app.baseURL + '/api/' + doc._id, data: doc}, callback)
+};
+
+viewModel.delete = function(doc) {
+    request({type: 'DELETE', url: app.baseURL + '/api/' + doc._id + '?rev=' + doc._rev}, function(error, data) {
+        viewModel.rows.remove(doc);
+    })
 }
 
 var observable = function(doc) {
-  var $save = function() {
-    save(ko.toJS(doc), function(error, data) {
-      doc._rev = data.rev;
-    })
-  };
-  doc.name = ko.observable(doc.name);
-  doc.name.subscribe($save);
-  doc.completed = ko.observable(doc.completed);
-  doc.completed.subscribe($save);
-  return doc;
+    var $save = function() {
+        viewModel.save(ko.toJS(doc), function(error, data) {
+            doc._rev = data.rev;
+        })
+    };
+    doc.name = ko.observable(doc.name);
+    doc.name.subscribe($save);
+    doc.completed = ko.observable(doc.completed);
+    doc.completed.subscribe($save);
+    doc.editing = ko.observable(false);
+    return doc;
 };
 
 request({url: app.baseURL + '/_view/foo'}, function(error, data) {
-  viewModel.rows = ko.observableArray();
-  $(data.rows).each(function(i, row) {
-    viewModel.rows.push(observable(row.value));
-  })
+    viewModel.rows = ko.observableArray();
+    $(data.rows).each(function(i, row) {
+        viewModel.rows.push(observable(row.value));
+    })
 
-  viewModel.rows.subscribe(save);
+    ko.applyBindings(viewModel);
 
-  ko.applyBindings(viewModel);
-
-
-  var input = document.getElementById('new');
-  var title = document.getElementById('title');
-  var list = document.getElementById('list');
-  var about = document.getElementById('about');
-  var create = document.getElementById('create');
-  var header = document.querySelector('header');
-  var footer = document.querySelector('footer');
-  var hash = window.location.hash;
-
-  header.style.display = 'block';
-  footer.style.visibility = 'visible'
+    $('footer').show();
 });
 
 
