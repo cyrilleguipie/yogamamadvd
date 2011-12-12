0d3b55fcd6a125003fdb9bb88c23a17c038b0a8c
azhdanov <azhdanov@47e8cd93-a071-7289-a944-3e509be4b2fb>
2011-12-12 00:27:20 +0000

    PPR, sample login

--- branches/play2/main/app/controllers/Application.scala
+++ branches/play2/main/app/controllers/Application.scala
@@ -2,12 +2,81 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-import models.User
+import play.api.data._
+
+import models._
+import views._
 
 object Application extends Controller {
   
-  def index = Action {
+  def Redirect(call: Call)(implicit request: RequestHeader): SimpleResult[Results.Empty] = {
+    var url = call.url;
+    if(request.queryString.contains("partial")) {
+      if (url.indexOf("?") > 0) {
+        url += "&partial"
+      } else {
+        url += "?partial"
+      }      
+    }
+    Status(play.api.http.Status.FOUND).withHeaders(LOCATION -> url)
+  }
+
+  def index = Action { implicit request =>
     Ok(views.html.index(User.findAll))
   }
-  
+
+  // -- Authentication
+
+  val loginForm = Form(
+    of(
+      "email" -> text,
+      "password" -> text
+    ) verifying ("Invalid email or password", result => result match {
+      case (email, password) => true
+    })
+  )
+
+  /**
+   * Login page.
+   */
+  def login = Action { implicit request =>
+    Ok(html.login(loginForm))
+  }
+
+  /**
+   * Handle login form submission.
+   */
+  def authenticate = Action { implicit request =>
+    loginForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(html.login(formWithErrors)),
+      user => Redirect(routes.Application.index).withSession("email" -> user._1)
+    )
+  }
+
+  /**
+   * Logout and clean the session.
+   */
+  def logout = Action { implicit request =>
+    Redirect(routes.Application.login).withNewSession.flashing(
+      "success" -> "You've been logged out"
+    )
+  }
+
+}
+
+/**
+ * Provide security features
+ */
+trait Secured extends Security.AllAuthenticated {
+
+  /**
+   * Retrieve the connected user email.
+   */
+  override def username(request: RequestHeader) = request.session.get("email")
+
+  /**
+   * Redirect to login if the use in not authorized.
+   */
+  override def onUnauthorized(request: RequestHeader) = Results.Redirect(routes.Application.login)
+
 }
